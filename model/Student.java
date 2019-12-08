5d954b269e6c10524f7d3129cc603872170827b6
curry301999 <51470726+curry301999@users.noreply.github.com>
2019-12-08 22:42:57 +0800

    Add files via upload

---
+++ model/Student.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.List;
+
+public class Student {
+
+	private Integer id;
+	private String name;
+	private String gender;
+	private List<Course> course;
+	private String password;
+	private String account;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	public List<Course> getCourse() {
+		return course;
+	}
+	public void setCourse(List<Course> course) {
+		this.course = course;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	
+}
---
+++ model/Teacher.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Teacher {
+
+	private Integer id;
+	private String name;
+	private String gender;
+	private String course;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	
+}
