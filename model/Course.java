3335d8a5109e2dc220c84939233a72469fa06b70
curry301999 <51470726+curry301999@users.noreply.github.com>
2019-12-08 22:44:20 +0800

    Add files via upload

---
+++ model/Course.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Course {
+
+	private Integer id;
+	private String name;
+	private String address;
+	private String time;
+	private String teacher;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void set(String name) {
+		this.name = name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	
+	
+}
